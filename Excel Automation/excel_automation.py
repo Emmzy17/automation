6b6f2a86f5053268db4f0629728f09cee66d1472
Emmzy17 <emmanueladjerry@gmail.com>
2022-01-10 13:22:17 -0800

    automation

--- Excel Automation/excel_automation.py
+++ Excel Automation/excel_automation.py
@@ -1,5 +1,6 @@
 import openpyxl
-
+from openpyxl.drawing.image import Image
+from openpyxl.chart import BarChart, Reference
 wb = openpyxl.load_workbook('sales.xlsx')
 sheets = wb.sheetnames
 userSheet = wb['Users']
@@ -9,9 +10,30 @@ saleSheet = wb['Sales']
 #wb.save('sales.xlsx')
 #wb.remove_sheet('new sheet1')
 #wb.save('sales.xlsx')
-dict_cell = userSheet._cells
-for column in userSheet.columns:
-    print(column[0].value,column[1].value, column[2].value,column[3].value)
+#dict_cell = userSheet._cells
+#for column in userSheet.columns:
+#    print(column[0].value,column[1].value, column[2].value,column[3].value)
+#print(userSheet['c6'].value)
+#userSheet['e1'] = 'New Total'
+ref = Reference(userSheet, min_col=3, min_row=2, max_col=3, max_row=11)
+chart = BarChart()
+chart.add_data(ref)
+userSheet.add_chart(chart, 'J6')
+
+for i in range(2, 12):
+    userSheet['E' + str(i)] = userSheet['C' + str(i)].value + 5
+userSheet['E12'] = '=SUM(E1:E11)'
+img= Image('new-logo-csk-2.png')
+img2 = Image('new-logo-csk-2.png')
+
+#saleSheet.merge_cells('C2:D3')
+
+
+
+#userSheet.add_image(img, 'B12')
+#userSheet.add_image(img2, 'E12')
+wb.save('sales.xlsx')
+
 
 print(saleSheet.dimensions)
 print(userSheet['b2'].value)
