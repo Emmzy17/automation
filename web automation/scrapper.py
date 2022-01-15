86920b28cffaf1cfbd1b07d83eb06960464769b9
Emmzy17 <emmanueladjerry@gmail.com>
2022-01-15 10:58:06 -0800

    web scarpping

--- web automation/scrapper.py
+++ web automation/scrapper.py
@@ -2,7 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 url = 'https://www.thenetnaija.co/music'
 html = requests.get(url)
-bsObj = BeautifulSoup(html.content,)
-h2 = bsObj.find('h2')
-print(bsObj.getText())
-print(h2.get_text())
+bsObj = BeautifulSoup(html.content)
+h2 = bsObj.find_next_sibling('h2')
+print(h2)
+#for mlist in h2:
+#    print(mlist.get_text())
+#print(bsObj.getText())
+#print(h2)
